67857ed8300c9db653922ed7a25a0a0a6369ddd8
Rajesh Roy <rajeshkumarroy01998@outlook.com>
2023-10-07 00:20:07 +0600

    initial Commit

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,101 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationContext _db;
+        public CategoryController(ApplicationContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+      
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            //if (obj.Name == obj.DisplayOrder.ToString())
+            //{
+            //    ModelState.AddModelError("name","The DisplayOrder can not exactly match the name");
+            //}
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category Created Successfully.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category categoryFrmDb = _db.Categories.Find(id);
+            if (categoryFrmDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFrmDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully.";
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Category categoryFrmDb = _db.Categories.Find(id);
+            if (categoryFrmDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFrmDb);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        public IActionResult EditPost(int? id)
+        {
+
+            Category? categoryFrmDb = _db.Categories.Find(id);
+            if (categoryFrmDb == null)
+            {
+                return NotFound();
+            }
+            _db.Categories.Remove(categoryFrmDb);
+            _db.SaveChanges();
+            TempData["success"] = "Category Created Successfully.";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BulkyWeb/Data/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
